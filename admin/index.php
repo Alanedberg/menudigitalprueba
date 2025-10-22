421d263125f13f4b9841e048f0961f644f165701
Alan Edberg <alanedberg19@gmail.com>
2025-10-22 02:01:28 -0300

    p

--- admin/index.php
+++ admin/index.php
@@ -4,6 +4,7 @@
 <head>
   <meta charset="utf-8" />
   <title>Admin · Menú Digital</title>
+  <base href="/menudigitalprueba/">
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <!-- Bootstrap 5 -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
