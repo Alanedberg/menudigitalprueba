3610dcaf61e9e48bf9555a0dfc6aefeb1b71de9b
Alan Edberg <alanedberg19@gmail.com>
2025-10-22 02:07:26 -0300

    f

--- admin/index.php
+++ admin/index.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8" />
   <title>Admin · Menú Digital</title>
-  <base href="/menudigitalprueba/admin/">
+  <base href="/menudigitalprueba/">
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <!-- Bootstrap 5 -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -231,6 +231,6 @@
   const API_BASE = '../api';
   const UPLOADS_BASE = '../uploads'; // ← ajusta si tu carpeta de imágenes es otra
 </script>
-<script src="./admin.js"></script>
+<script src=".admin/admin.js"></script>
 </body>
 </html>
